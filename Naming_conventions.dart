e7937029fceca68960fc2accec156952ad500bc7
Rabia Abubackar <112841839+Rabia-Abubackar@users.noreply.github.com>
2023-05-09 09:50:51 +0530

    Add files via upload

---
+++ Naming_conventions.dart
@@ -0,0 +1,21 @@
+void main()
+{
+  String pizzaName = "Dominator Pizza";
+  int pizzaPrize = 320;
+  final gstDominator = 350;
+  print("Dominos speacial pizza ${pizzaName} RS: ${pizzaPrize} with GST ${gstDominator}");
+
+  const pi = 3.14;
+  for(int i=0; i<=10; i++)
+  {
+    final count = i;
+    print(count);
+  }
+
+  int a = 10;
+  int b = 20;
+  double finalTotal = (a+b)*pi;
+  final total = finalTotal;
+  print(finalTotal);
+}
+
